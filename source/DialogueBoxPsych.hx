8467982334a94c6abf1566d46018d1ddc6d1e337
Kauan <98900217+Kauan3323@users.noreply.github.com>
2022-10-14 15:41:42 -0300

    Update DialogueBoxPsych.hx

--- source/DialogueBoxPsych.hx
+++ source/DialogueBoxPsych.hx
@@ -332,7 +332,20 @@ class DialogueBoxPsych extends FlxSpriteGroup
 			bgFade.alpha += 0.5 * elapsed;
 			if(bgFade.alpha > 0.5) bgFade.alpha = 0.5;
 
-			if(PlayerSettings.player1.controls.ACCEPT) {
+			#if android
+                        var justTouched:Bool = false;
+
+		        for (touch in FlxG.touches.list)
+		        {
+			        if (touch.justPressed)
+			        {
+				        justTouched = true;
+			        }
+		        }
+		        #end
+
+			if(PlayerSettings.player1.controls.ACCEPT #if android || justTouched #end) {
+			
 				if(!daText.finishedText) {
 					if(daText != null) {
 						daText.killTheTimer();
@@ -569,7 +582,7 @@ class DialogueBoxPsych extends FlxSpriteGroup
 	}
 
 	public static function parseDialogue(path:String):DialogueFile {
-		#if MODS_ALLOWED
+		#if desktop
 		if(FileSystem.exists(path))
 		{
 			return cast Json.parse(File.getContent(path));
